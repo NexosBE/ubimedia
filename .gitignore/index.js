048dca8a066f5d35d69dd43e11d7c2f0a7f2ad94
NexosBE <42642535+NexosBE@users.noreply.github.com>
2018-08-23 22:41:27 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
-bot.login("NDc5NjY2NjUzOTcwMzAwOTQ5.DmAtTA.uLyD2jDEXZu9OlpVf-gy7PuTfMI")
+bot.login(process.env.TOKEN)
 var prefix = ("?")
 
 bot.on('ready', function() {
