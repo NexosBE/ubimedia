573db18c1cc4e7a702e08d69846f8b7418bd9335
NexosBE <42642535+NexosBE@users.noreply.github.com>
2018-08-23 19:30:26 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,24 +1,25 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
-
+bot.login("NDc5NjY2NjUzOTcwMzAwOTQ5.DmAtTA.uLyD2jDEXZu9OlpVf-gy7PuTfMI")
 var prefix = ("?")
 
 bot.on('ready', function() {
-    bot.user.setPresence({ game: { name: 'En développement...', type: 0} })
+    bot.user.setPresence({ game: { name: 'En développement..', type: 0} })
     console.log("Connected");
 });
 
-bot.login("NDc5NjY2NjUzOTcwMzAwOTQ5.DmAtTA.uLyD2jDEXZu9OlpVf-gy7PuTfMI")
-
-
-bot.on('message', message => {
-    if (message.content === prefix + "help"){
-        message.channel.sendMessage("Liste des commandes : \n -?help");
-    }
-
 bot.on("guildMemberAdd", member => {
-    member.guild.channels.find("name", "🙌aéroport").send(`Bienvenue ${member} sur **UbiMedia - Discord Officiel !`);
+    const bvn = member.guild.channels.find(m => m.name === "🙌aéroport")
+    if (!bvn) return;
+    const embed = new Discord.RichEmbed()
+    .setColor('#009114')
+    .setAuthor(member.user.tag, member.user.avatarURL)
+    .setTitle("Arrivée d'un nouvel utilisateur")
+    .addField("Un nouvel utilisateur vient d'arriver", `Il s'agit de ${member.user.tag}`, true)
+    .setDescription("J'éspère que tu te plairas a nos côté")
+    .addField(`Nombre de membres après l'arrivée de __${member.user.tag}__`, member.guild.memberCount)
+    .setFooter(`ID : ${member.user.ud}`)
+    .setTimestamp()
+    bvn.send(embed)
 })
 
-
-})
