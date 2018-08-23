66cd1088d3c869d1208341213dfa6b7f50520ee6
NexosBE <42642535+NexosBE@users.noreply.github.com>
2018-08-23 19:10:28 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -4,7 +4,7 @@ const bot = new Discord.Client();
 var prefix = ("?")
 
 bot.on('ready', function() {
-    bot.user.setPresence({ game: { name: 'Bot in dev...', type: 0} })
+    bot.user.setPresence({ game: { name: 'En développement...', type: 0} })
     console.log("Connected");
 });
 
@@ -15,4 +15,10 @@ bot.on('message', message => {
     if (message.content === prefix + "help"){
         message.channel.sendMessage("Liste des commandes : \n -?help");
     }
+
+bot.on("guildMemberAdd", member => {
+    member.guild.channels.find("name", "🙌aéroport").send(`Bienvenue ${member} sur **UbiMedia - Discord Officiel !`);
+})
+
+
 })
